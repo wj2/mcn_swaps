38d189edce2d057ba59a9267feebb0f2d300f2c6
wjj <wjeffreyjohnston@gmail.com>
2024-08-06 16:26:54 -0400

    Added models with different recurrent systems

--- nets.py
+++ nets.py
@@ -2,16 +2,26 @@
 import torch.nn as nn
 
 
-class SimpleNet(nn.Module):
-    def __init__(self, inp_size, num_h, out_size):
-        super(SimpleNet, self).__init__()
+class SimpleRecurrent(nn.Module):
+    def __init__(self, inp_size, num_h, out_size, net_type=nn.RNN):
+        super(SimpleRecurrent, self).__init__()
         self.hidden_dim = num_h
         self.out_dim = out_size
         self.in_dim = inp_size
-        self.lstm = nn.LSTM(inp_size, num_h)
+        self.recurrent_net = net_type(inp_size, num_h)
         self.linear = nn.Linear(num_h, out_size)
 
     def forward(self, x):
-        out, hidden = self.lstm(x)
+        out, hidden = self.recurrent_net(x)
         x = self.linear(out)
         return x
+
+
+class SimpleRNN(SimpleRecurrent):
+    def __init__(self, *args, **kwargs):
+        super(SimpleRNN, self).__init__(*args, **kwargs, net_type=nn.RNN)
+        
+
+class SimpleLSTM(SimpleRecurrent):
+    def __init__(self, *args, **kwargs):
+        super(SimpleLSTM, self).__init__(*args, **kwargs, net_type=nn.LSTM)
